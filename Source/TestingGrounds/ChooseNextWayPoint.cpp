a195d325c0d144151fbe17e2144bce10833b5870
StaticTJO <sk8terboy6931@gmail.com>
2018-05-01 17:20:37 -0600

    Patrolling Guard in C++ works fine Will crash if Patrol Array is Empty. TO DO

--- Source/TestingGrounds/ChooseNextWayPoint.cpp
+++ Source/TestingGrounds/ChooseNextWayPoint.cpp
@@ -1,19 +1,47 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWayPoint.h"
+#include "PatrollingGuard.h"
+#include "AIController.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	Super::ExecuteTask(OwnerComp, NodeMemory);
-
-
 	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
-	//Index Key is a struct, the get value returns the index number of the data stored in the blackboard, Takes an FName String that is taken from
-	// The drop down.
+	auto PatrolPoints = GetControlPoints(OwnerComp);
 	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint index:%i"),Index);
-	return EBTNodeResult::Succeeded;
+	if (PatrolPoints[Index] != nullptr) {
+
+		SetNextWayPoint(PatrolPoints, Index, BlackBoardComp);
+		CycleIndex(Index, PatrolPoints, BlackBoardComp);
+		//Index Key is a struct, the get value returns the index number of the data stored in the blackboard, Takes an FName String that is taken from
+		// The drop down.
+		return EBTNodeResult::Succeeded;
+	}
+	else {
+
+		return EBTNodeResult::Failed;
+	}
+}
+
+
+TArray<AActor*> UChooseNextWayPoint::GetControlPoints(UBehaviorTreeComponent& OwnerComp) {
+	// Need AI controller Include To GetPawn
+	auto Guard = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
+	return Guard->GetPatrolPoints();
+}
+
+void UChooseNextWayPoint::SetNextWayPoint(TArray<AActor*> &PatrolPoints,int32& Index, UBlackboardComponent* BlackBoardComp) {
+	auto NextWayPoint = PatrolPoints[Index];
+	BlackBoardComp->SetValueAsObject(WayPoint.SelectedKeyName, NextWayPoint);
+}
+
+void UChooseNextWayPoint::CycleIndex(int32& Index, TArray<AActor*> &PatrolPoints, UBlackboardComponent* BlackBoardComp) {
+	int32 ModIndex;
+	Index++;
+	ModIndex = Index % PatrolPoints.Num();
+	BlackBoardComp->SetValueAsInt(IndexKey.SelectedKeyName, ModIndex);
 }
 
 
--- Source/TestingGrounds/ChooseNextWayPoint.h
+++ Source/TestingGrounds/ChooseNextWayPoint.h
@@ -6,6 +6,8 @@
 #include "BehaviorTree/BTTaskNode.h"
 #include "ChooseNextWayPoint.generated.h"
 
+class APatrollingGuard;
+
 /**
  * 
  */
@@ -15,11 +17,18 @@ class TESTINGGROUNDS_API UChooseNextWayPoint : public UBTTaskNode
 	GENERATED_BODY()
 
 public:
-
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 
 protected:
 	//Only Children See this
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
-		struct FBlackboardKeySelector IndexKey;
+	struct FBlackboardKeySelector IndexKey;
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WayPoint;
+
+private:
+	APatrollingGuard* PatrollingGuard = nullptr;
+	TArray<AActor*> GetControlPoints(UBehaviorTreeComponent& OwnerComp);
+	void SetNextWayPoint(TArray<AActor*> &PatrolPoints,int32 &Index, UBlackboardComponent* BlackBoardComp);
+	void CycleIndex(int32& Index, TArray<AActor*> &PatrolPoints, UBlackboardComponent* BlackBoardComp);
 };
