2dd4b69eaf8de31cf8f69ccd6d297ea6f3b0f836
staticjpl <sk8terboy6931@hotmail.com>
2018-05-25 03:28:44 +0300

    Refactored and added Gun code

--- Source/TestingGrounds/Weapons/Gun.h
+++ Source/TestingGrounds/Weapons/Gun.h
@@ -15,10 +15,35 @@ public:
 	// Sets default values for this actor's properties
 	AGun();
 
+    /** Gun mesh: 1st person view (seen only by self) */
+    UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+    class USkeletalMeshComponent* FP_Gun;
+
+    /** Location on gun mesh where projectiles should spawn. */
+    UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+    class USceneComponent* FP_MuzzleLocation;
+
+    /** Projectile class to spawn */
+    UPROPERTY(EditDefaultsOnly, Category=Projectile)
+    TSubclassOf<class ABallProjectile> ProjectileClass;
+
+    /** Sound to play each time we fire */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+    class USoundBase* FireSound;
+
+    /** AnimMontage to play each time we fire */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+    class UAnimMontage* FireAnimation;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+    class UAnimInstance* AnimInstance;
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+    /* Fires a projectile. */
+    void OnFire();
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
