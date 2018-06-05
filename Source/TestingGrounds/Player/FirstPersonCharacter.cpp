b294741f54a2b22db52eb0e7ef80ba9b96ed2405
staticjpl <sk8terboy6931@hotmail.com>
2018-06-05 17:28:59 +0300

    Reconfigured Gun

--- Source/TestingGrounds/Player/FirstPersonCharacter.cpp
+++ Source/TestingGrounds/Player/FirstPersonCharacter.cpp
@@ -62,9 +62,15 @@ void AFirstPersonCharacter::BeginPlay()
 
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
     //TODO FP_Gun AttachToComponent
-
     Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
     Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+    Gun->AnimInstance = Mesh1P->GetAnimInstance();
+    // Bind fire event TODO
+
+    if (EnableTouchscreenMovement(InputComponent) == false)
+    {
+        InputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
+    }
 
 }
 
@@ -79,9 +85,6 @@ void AFirstPersonCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 	// Bind jump events
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
-
-    // Bind fire event TODO
-    //PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFirstPersonCharacter::OnFire);
 
 	// Enable touchscreen input
 	EnableTouchscreenMovement(PlayerInputComponent);
--- Source/TestingGrounds/Weapons/Gun.h
+++ Source/TestingGrounds/Weapons/Gun.h
@@ -38,12 +38,13 @@ public:
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
     class UAnimInstance* AnimInstance;
 
+    /* Fires a projectile. */
+    void OnFire();
+
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-    /* Fires a projectile. */
-    void OnFire();
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
